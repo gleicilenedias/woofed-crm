83ee397965e6b88218b510f9e2ce916110cddc12
Yukio Arie <yukioarie@gmail.com>
2025-08-13 19:31:51 -0300

    Refactor Deal Products api tests

--- spec/controllers/api/accounts/deal_products_controller_spec.rb
+++ spec/controllers/api/accounts/deal_products_controller_spec.rb
@@ -7,84 +7,85 @@ RSpec.describe 'Deal Products API', type: :request do
   let!(:user) { create(:user, account:) }
   let!(:product) { create(:product, account:) }
   let!(:contact) { create(:contact, account:) }
-  let!(:pipeline) { create(:pipeline, account:) }
-  let!(:stage) { create(:stage, account:, pipeline:) }
-  let!(:deal) { create(:deal, account:, contact:, stage:) }
+  let!(:deal) { create(:deal, account:, contact:) }
   let!(:deal_product) { create(:deal_product, account:, deal:, product:) }
   let(:last_deal_product) { DealProduct.last }
+  let(:last_event) { Event.last }
+  let(:auth_headers) { { 'Authorization': "Bearer #{user.get_jwt_token}", 'Content-Type': 'application/json' } }
 
-  describe 'POST /api/v1/accounts/{account.id}/deal_products' do
-    let(:another_product) { create(:product, account:) }
-
-    let(:params) { { deal_id: deal.id, product_id: another_product.id, quantity: 2 } }
-
+  describe 'POST /api/v1/accounts/:account_id/deal_products' do
     context 'when it is an unauthenticated user' do
       it 'returns unauthorized' do
-        expect do
-          post "/api/v1/accounts/#{account.id}/deal_products", params:
-        end.not_to change(DealProduct, :count)
-
+        post "/api/v1/accounts/#{account.id}/deal_products", params: {}
         expect(response).to have_http_status(:unauthorized)
       end
     end
 
     context 'when it is an authenticated user' do
-      it 'create deal_product' do
+      let(:another_product) do
+        create(:product, account:, name: 'Product B', identifier: 'PROD_B', amount_in_cents: 2000)
+      end
+      let(:params) { { deal_id: deal.id, product_id: another_product.id, quantity: 2 }.to_json }
+
+      it 'creates the deal_product and deal_product_added event' do
         expect do
-          post "/api/v1/accounts/#{account.id}/deal_products",
-               headers: { 'Authorization': "Bearer #{user.get_jwt_token}" },
-               params:
+          post "/api/v1/accounts/#{account.id}/deal_products", params:, headers: auth_headers
         end.to change(DealProduct, :count).by(1)
                                           .and change(Event, :count).by(1)
-        expect(response).to have_http_status(:success)
-        total_amount_in_cents = last_deal_product.quantity * another_product.amount_in_cents
-        expect(last_deal_product.product_identifier).to eq(another_product.identifier)
-        expect(last_deal_product.product_name).to eq(another_product.name)
-        expect(last_deal_product.total_amount_in_cents).to eq(total_amount_in_cents)
-        expect(last_deal_product.unit_amount_in_cents).to eq(another_product.amount_in_cents)
-        expect(last_deal_product.quantity).to eq(2)
-        expect(DealProduct.count).to eq(2)
+        expect(response).to have_http_status(:created)
+        result = JSON.parse(response.body)
+        expect(result['product']['name']).to eq('Product B')
+        expect(result['product']['identifier']).to eq('PROD_B')
+        expect(result['deal']['id']).to eq(deal.id)
+        expect(result['quantity']).to eq(2)
+        expect(result['total_amount_in_cents']).to eq(4000) # 2 * 2000
+        expect(result['unit_amount_in_cents']).to eq(2000)
+        expect(last_event.kind).to eq('deal_product_added')
+        expect(last_deal_product.product_identifier).to eq('PROD_B')
+        expect(last_deal_product.product_name).to eq('Product B')
+        expect(last_deal_product.account).to eq(account)
       end
-      context 'when params is not valid' do
-        let(:params) { { product_id: product.id } }
-        it 'should raise an error' do
-          expect do
-            post "/api/v1/accounts/#{account.id}/deal_products",
-                 headers: { 'Authorization': "Bearer #{user.get_jwt_token}" },
-                 params:
-          end.to change(DealProduct, :count).by(0)
 
+      context 'when params are invalid' do
+        it 'returns unprocessable_entity with errors' do
+          params = { deal_id: nil, product_id: nil }.to_json
+          expect do
+            post "/api/v1/accounts/#{account.id}/deal_products", params:, headers: auth_headers
+          end.to change(DealProduct, :count).by(0)
           expect(response).to have_http_status(:unprocessable_entity)
-          expect(DealProduct.count).to eq(1)
+          expect(JSON.parse(response.body)['errors']).to include('Deal must exist', 'Product must exist')
         end
       end
     end
   end
 
-  describe 'GET /api/v1/accounts/{account.id}/deal_products/{product.id}' do
+  describe 'GET /api/v1/accounts/:account_id/deal_products/:id' do
     context 'when it is an unauthenticated user' do
       it 'returns unauthorized' do
         get "/api/v1/accounts/#{account.id}/deal_products/#{deal_product.id}"
-
         expect(response).to have_http_status(:unauthorized)
       end
     end
 
     context 'when it is an authenticated user' do
-      context 'get deal_product' do
-        it 'should return deal_product' do
-          get "/api/v1/accounts/#{account.id}/deal_products/#{deal_product.id}",
-              headers: { 'Authorization': "Bearer #{user.get_jwt_token}" }
+      it 'returns deal_product with included product and deal' do
+        get "/api/v1/accounts/#{account.id}/deal_products/#{deal_product.id}", headers: auth_headers
+        expect(response).to have_http_status(:ok)
+        result = JSON.parse(response.body)
+        expect(result['product']['name']).to eq(product.name)
+        expect(result['product']['identifier']).to eq(product.identifier)
+        expect(result['deal']['id']).to eq(deal.id)
+        expect(result['quantity']).to eq(1)
+        expect(result['total_amount_in_cents']).to eq(deal_product.total_amount_in_cents)
+        expect(result['unit_amount_in_cents']).to eq(deal_product.unit_amount_in_cents)
+      end
 
-          expect(response).to have_http_status(:success)
-          expect(response.body).to include(deal_product.product.name.to_s)
-          expect(response.body).to include(deal_product.deal.name.to_s)
-        end
-        it 'when deal_product is not found' do
-          get "/api/v1/accounts/#{account.id}/deal_products/1",
-              headers: { 'Authorization': "Bearer #{user.get_jwt_token}" }
-
+      context 'when deal_product is not found ' do
+        it 'returns not found' do
+          get "/api/v1/accounts/#{account.id}/deal_products/465465465465465465645",
+              headers: auth_headers
           expect(response).to have_http_status(:not_found)
+          expect(JSON.parse(response.body)).to eq({ 'errors' => 'Not found' })
         end
       end
     end
