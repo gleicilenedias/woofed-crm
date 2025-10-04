2fe31cabefe83247761c8fba279157b2a92aa8d7
Yukio Arie <yukioarie@gmail.com>
2025-10-04 14:36:06 -0300

    Correct contacts chatwoot_id index in additional_attributes for faster queries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_26_060748) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_04_171840) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -132,7 +132,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_26_060748) do
     t.bigint "app_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index "((additional_attributes ->> 'chatwoot_id'::text))", name: "index_contacts_on_chatwoot_id", where: "((additional_attributes -> 'chatwoot_id'::text) IS NOT NULL)"
+    t.index "((additional_attributes ->> 'chatwoot_id'::text)), id", name: "index_contacts_on_chatwoot_id"
     t.index "NULLIF((phone)::text, ''::text)", name: "index_contacts_on_phone", unique: true
     t.index "lower(NULLIF((email)::text, ''::text))", name: "index_contacts_on_lower_email", unique: true
     t.index ["app_type", "app_id"], name: "index_contacts_on_app"
