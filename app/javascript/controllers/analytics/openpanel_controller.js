183749eea6e875a3898313866aa6269b7c362c37
Yukio Arie <yukioarie@gmail.com>
2025-10-27 22:41:06 -0300

    Install and use openpanel analytics via yarn

---
+++ app/javascript/controllers/analytics/openpanel_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from "stimulus";
+import { OpenPanel } from "@openpanel/web";
+import { getMetaJSON, getMetaContent, getRailsEnv } from "../../utils/meta";
+
+export default class extends Controller {
+  connect() {
+    if (window.op) return;
+
+    const railsEnv = getRailsEnv();
+    const endpoint = getMetaContent("openpanel-endpoint");
+    const token = getMetaContent("openpanel-token");
+
+    if (!endpoint || !token || railsEnv !== "production") return;
+
+    const userData = getMetaJSON("user-data");
+
+    window.op = new OpenPanel({
+      apiUrl: endpoint,
+      clientId: token,
+      disabled: false,
+      trackScreenViews: true,
+      trackOutgoingLinks: true,
+      trackAttributes: true,
+    });
+
+    if (userData && userData.id) {
+      window.op.identify({
+        profileId: userData.id,
+        firstName: userData.full_name,
+        email: userData.email,
+        properties: {
+          account_id: userData.account_id,
+          account_name: userData.account_name,
+        },
+      });
+    }
+  }
+}
