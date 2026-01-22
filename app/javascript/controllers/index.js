e0fbda32888eb95b494dd2739ea70b3deadbd152
Yukio Arie <yukioarie@gmail.com>
2026-01-22 19:40:50 -0300

    Update stimulus js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,6 @@
 // Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js or *_controller.ts.
-
-import { Application } from "stimulus";
+import { Application } from "@hotwired/stimulus";
 import { registerControllers } from "stimulus-vite-helpers";
 
 const application = Application.start();
